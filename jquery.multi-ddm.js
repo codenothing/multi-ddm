295290210d39eaa9231f88330b8bde62cf19b3c5
corey <corey@codenothing.com>
2010-02-19 01:15:57 -0800

    Adding v2.0

--- jquery.multi-ddm.js
+++ jquery.multi-ddm.js
@@ -1,20 +1,32 @@
-/**
- * Multi-level Drop Down Menu 2.0
- * August 22, 2009
+/*!
+ * Multi-level Drop Down Menu 2.1
+ * October 1, 2009
  * Corey Hart @ http://www.codenothing.com
+ *
+ * Credit to Shaun Johnson for pointing out the Prototype confliction, and IE6 bgiframe fix
  */ 
-;(function($){
-	// Needed for IE Compatibility
-	$.fn.reverse = [].reverse;
+;(function($, undefined){
+	// Needed for IE Compatibility (Closing menus must be done backwards in IE)
+	// Ensure that no complications arise from other libraries modifiying the 
+	// array functionality (and hope that they store the old reverse function into _reverse)
+	$.fn.reverse = []._reverse||[].reverse;
 
+	// bgiframe is needed to fix z-index problem for IE6 users.
+	$.fn.bgiframe = $.fn.bgiframe ? $.fn.bgiframe : $.fn.bgIframe ? $.fn.bgIframe : function(){
+		// For applications that don't have bgiframe plugin installed, create a useless 
+		// function that doesn't break the chain
+		return this;
+	};
+
+	// Drop Down Plugin
 	$.fn.dropDownMenu = function(options){
 		return this.each(function(){
 			// Defaults with metadata support
 			var $mainObj = $(this), menus = [], classname, timeout, $obj, $obj2, 
 				settings = $.extend({
 					timer: 500,
-					parentMO: null,
-					childMO: null,
+					parentMO: undefined,
+					childMO: undefined,
 					levels: [],
 					numberOfLevels: 5
 				}, options||{}, $.metadata ? $mainObj.metadata() : {});
@@ -36,7 +48,7 @@
 				menus[i] = menus[i-1].children('ul').children('li');
 	
 				// Action
-				menus[i-1].mouseover(function(){
+				menus[i-1].bind('mouseover.multi-ddm', function(){
 					// Defaults
 					$obj = $(this); $obj2 = $obj.children('a');
 	
@@ -53,8 +65,8 @@
 	
 					// Hide open menus and open current menu
 					$obj.siblings('li').find('ul:visible').reverse().hide();
-					$obj2.addClass( $obj2.data('classname') ).siblings('ul').show();
-				}).mouseout(function(){
+					$obj2.addClass( $obj2.data('classname') ).siblings('ul').bgiframe().show();
+				}).bind('mouseout.multi-ddm', function(){
 					if ($(this).children('a').data('classname') == settings.levels[0])
 						timeout = setTimeout(closemenu, settings.timer);
 				}).children('a').data('classname', classname);
